5fd1951e85a80d533c0c4a45a5e15033da555ad8
ChrisCogs <ccogs@yahoo.com>
2019-06-24 21:37:30 -0400

    Added models and modified the server file to use mongoose.

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 var express = require("express");
-var mongojs = require("mongojs");
+// var mongojs = require("mongojs");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var logger = require("morgan");
 const mongoose = require('mongoose');
  
 // mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 
+var db = require("./models");
+
+var PORT = 3000;
+
 // Initialize Express
 var app = express();
 
-// Database Configuration
-var databaseURL = "mongoLion";
-var collections = ["huntedData"];
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-var db = mongojs(databaseURL, collections);
-db.on("error", (error) => {
-    console.log("Database Error:" ,error);
-});
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoLion";
+
+mongoose.connect(MONGODB_URI);
 
 app.get("/", (req, res) => {
     res.send("Welcome to Mongo-Lion");
@@ -38,37 +46,78 @@ app.get("/scrape", (req, res) => {
         var $ = cheerio.load(response.data);
 
         $("header").each(function(i, element) {
-            // Save the text and href of each link enclosed in the current element
-            // console.log(element);
-            var title = $(element).children("h2").children("a").text();
-            var link = $(element).children("h2").children("a").attr("href");
+            // Save an empty result object
+            var result = {};
+      
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+              .children("h2")
+              .children("a")
+              .text();
 
-            console.log(title);
-            console.log(link);
-            
-             // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the scrapedData db
-        db.huntedData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
+            result.summary = $(this)
+              .children("p.excerpt")  
+              .text();
+              
+            result.link = $(this)
+              .children("h2")
+              .children("a")
+              .attr("href");
+             
+
+      
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+              .then(function(dbArticle) {
+                // View the added result in the console
+                console.log(dbArticle);
+              })
+              .catch(function(err) {
+                // If an error occurred, log it
+                console.log(err);
+              });
+          });
+      
+          // Send a message to the client
+          res.send("Scrape Complete");
         });
-      }
-     });
-  });
-    // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
+      });
+
+
+
+
+//         $("header").each(function(i, element) {
+//             // Save the text and href of each link enclosed in the current element
+//             // console.log(element);
+            // var title = $(element).children("h2").children("a").text();
+            // var link = $(element).children("h2").children("a").attr("href");
+
+//             console.log(title);
+//             console.log(link);
+            
+//              // If this found element had both a title and a link
+//       if (title && link) {
+//         // Insert the data in the scrapedData db
+//         db.huntedData.insert({
+//           title: title,
+//           link: link
+//         },
+//         function(err, inserted) {
+//           if (err) {
+//             // Log the error if one is encountered during the query
+//             console.log(err);
+//           }
+//           else {
+//             // Otherwise, log the inserted data
+//             console.log(inserted);
+//           }
+//         });
+//       }
+//      });
+//   });
+//     // Send a "Scrape Complete" message to the browser
+//   res.send("Scrape Complete");
+// });
 
 
 
@@ -76,6 +125,6 @@ app.get("/scrape", (req, res) => {
 
 
 
-app.listen(3000, function() {
-    console.log("App running on port 3000!");
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
   });
